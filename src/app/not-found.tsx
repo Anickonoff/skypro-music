e23302ac7d3ad51a4bfde2b94d96251911f9f381
Anton Nikonov <anick.on.off@gmail.com>
2026-05-29 22:59:03 +0700

    add not-found page layout according to the design

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,53 @@
+'use client';
+
 import Link from 'next/link';
+import styles from './not-found.module.css';
+import Nav from '@/components/Nav/Nav';
+import Sidebar from '@/components/Sidebar/Sidebar';
+import Bar from '@/components/Bar/Bar';
+import { useState } from 'react';
+import Search from '@/components/Search/Search';
+import { useRouter } from 'next/navigation';
 
 export default function notFound() {
-  return <Link href={'/music/main'}>На главную</Link>;
+  const router = useRouter();
+  const [searchQuery, setSearchQuery] = useState(''); // состояние для хранения поискового запроса
+  const onSearchInputChange = (query: string) => {
+    setSearchQuery(query);
+  }; // функция для обновления поискового запроса
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <Nav />
+          <div className={styles.notFound}>
+            <Search
+              onInputChange={onSearchInputChange}
+              inputValue={searchQuery}
+            />
+            <div className={styles.notFound_wrapper}>
+              <h1 className={styles.notFound_title}>404</h1>
+              <div className={styles.notFound_subtitle}>
+                <p>Страница не найдена</p>
+                <img src="img/smile_crying.png" alt="Грустный смайлик" />
+              </div>
+              <p className={styles.notFound_text}>
+                Возможно она была удалена или перенесена на другой адрес
+              </p>
+              <button
+                className={styles.notFound_btn}
+                onClick={() => {
+                  router.push('/music/main');
+                }}
+              >
+                Вернуться на главную
+              </button>
+            </div>
+          </div>
+          <Sidebar showPlaylists={false} />
+        </main>
+        <Bar />
+      </div>
+    </div>
+  );
 }
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -6,7 +6,11 @@ import { useAppDispatch, useAppSelector } from '@/store/store';
 import { clearUserData } from '@/store/features/authSlice';
 import Skeleton from 'react-loading-skeleton';
 
-export default function Sidebar() {
+export default function Sidebar({
+  showPlaylists = true,
+}: {
+  showPlaylists: boolean;
+}) {
   const username = useAppSelector((state) => state.auth.username);
   const dispatch = useAppDispatch();
   const { fetching } = useAppSelector((state) => state.track);
@@ -26,55 +30,57 @@ export default function Sidebar() {
           </svg>
         </div>
       </div>
-      <div className={styles.sidebar__block}>
-        <div className={styles.sidebar__list}>
-          <div className={styles.sidebar__item}>
-            {fetching ? (
-              <Skeleton width={250} height={150} />
-            ) : (
-              <Link className={styles.sidebar__link} href="/music/category/2">
-                <img
-                  className={styles.sidebar__img}
-                  src="/img/playlist01.png"
-                  alt="day's playlist"
-                  width={250}
-                  height={150}
-                />
-              </Link>
-            )}
-          </div>
-          <div className={styles.sidebar__item}>
-            {fetching ? (
-              <Skeleton width={250} height={150} />
-            ) : (
-              <Link className={styles.sidebar__link} href="/music/category/3">
-                <img
-                  className={styles.sidebar__img}
-                  src="/img/playlist02.png"
-                  alt="100 dance hits"
-                  width={250}
-                  height={150}
-                />
-              </Link>
-            )}
-          </div>
-          <div className={styles.sidebar__item}>
-            {fetching ? (
-              <Skeleton width={250} height={150} />
-            ) : (
-              <Link className={styles.sidebar__link} href="/music/category/4">
-                <img
-                  className={styles.sidebar__img}
-                  src="/img/playlist03.png"
-                  alt="indie charge"
-                  width={250}
-                  height={150}
-                />
-              </Link>
-            )}
+      {showPlaylists && (
+        <div className={styles.sidebar__block}>
+          <div className={styles.sidebar__list}>
+            <div className={styles.sidebar__item}>
+              {fetching ? (
+                <Skeleton width={250} height={150} />
+              ) : (
+                <Link className={styles.sidebar__link} href="/music/category/2">
+                  <img
+                    className={styles.sidebar__img}
+                    src="/img/playlist01.png"
+                    alt="day's playlist"
+                    width={250}
+                    height={150}
+                  />
+                </Link>
+              )}
+            </div>
+            <div className={styles.sidebar__item}>
+              {fetching ? (
+                <Skeleton width={250} height={150} />
+              ) : (
+                <Link className={styles.sidebar__link} href="/music/category/3">
+                  <img
+                    className={styles.sidebar__img}
+                    src="/img/playlist02.png"
+                    alt="100 dance hits"
+                    width={250}
+                    height={150}
+                  />
+                </Link>
+              )}
+            </div>
+            <div className={styles.sidebar__item}>
+              {fetching ? (
+                <Skeleton width={250} height={150} />
+              ) : (
+                <Link className={styles.sidebar__link} href="/music/category/4">
+                  <img
+                    className={styles.sidebar__img}
+                    src="/img/playlist03.png"
+                    alt="indie charge"
+                    width={250}
+                    height={150}
+                  />
+                </Link>
+              )}
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
